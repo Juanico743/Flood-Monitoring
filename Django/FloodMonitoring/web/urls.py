e8ab1175060ac0e16d0c5943453529c19a6d81af
Vincent Jerry V. Juanico <vincentjerryjuanico@gmail.com>
2026-04-04 11:33:19 +0800

    040426: Add pages for CRUD of Sensor, Vehicle Threshold, and Emergency Contact

--- Django/FloodMonitoring/web/urls.py
+++ Django/FloodMonitoring/web/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import login_view, dashboard_view, logout_view
+from .views import login_view, dashboard_view, logout_view, sensors_crud_view, threshold_crud_view, emergency_crud_view
 
 urlpatterns = [
     path('', login_view, name='login'),
     path('dashboard/', dashboard_view, name='dashboard'),
 
     path('logout/', logout_view, name='logout'),
+
+    path('sensors/', sensors_crud_view, name='sensor_crud'),
+    path('thresholds/', threshold_crud_view, name='threshold_crud'),
+    path('emergency-contacts/', emergency_crud_view, name='contact_crud'),
 ]
--- Django/FloodMonitoring/web/views.py
+++ Django/FloodMonitoring/web/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from api.models import AdminAuthentication, Sensor
+from api.models import AdminAuthentication, Sensor, VehicleFloodThreshold, EmergencyContact
 from django.utils import timezone
 from datetime import datetime
+
 
 def login_view(request):
     if 'admin_id' in request.session:
@@ -52,3 +53,42 @@ def logout_view(request):
     request.session.flush() 
     return redirect('login')
 
+
+
+
+
+def sensors_crud_view(request):
+    if 'admin_id' not in request.session:
+        return redirect('login')
+
+    sensors = Sensor.objects.all()
+    username = request.session.get('username', 'Admin') 
+    
+    return render(request, 'web/data_management/sensors_crud.html', {
+        'sensors': sensors,
+        'username': username  
+    })
+
+def threshold_crud_view(request):
+    if 'admin_id' not in request.session:
+        return redirect('login')
+
+    thresholds = VehicleFloodThreshold.objects.all()
+    username = request.session.get('username', 'Admin')
+    
+    return render(request, 'web/data_management/thresholds_crud.html', {
+        'thresholds': thresholds,
+        'username': username  
+    })
+
+def emergency_crud_view(request):
+    if 'admin_id' not in request.session:
+        return redirect('login')
+        
+    contacts = EmergencyContact.objects.all()
+    username = request.session.get('username', 'Admin')
+    
+    return render(request, 'web/data_management/emergency_crud.html', {
+        'emergencyContacts': contacts,
+        'username': username  
+    })
